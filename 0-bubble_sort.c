b8aa7fa0d1b58d1981716dd43f9fa41b8b613c3f
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-06-15 23:49:39 +0000

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,8 +1,27 @@
 #include "sort.h"
 
 /**
- * 
- * 
+ * bubble_sort - function to order whyt the method bubble
+ *@array: value
+ *@size: value
+ * print_function - function to be printed
  * */
 void bubble_sort(int *array, size_t size)
-{}
+{
+	size_t i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < size - i ; j++)
+		{
+			if (array[j - 1] > array[j])
+			{
+				k = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = k;
+				print_array(array, size);
+			}
+		}
+
+	}
+}
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,9 +1,37 @@
 #include "sort.h"
 
 /**
- * 
- * 
- * 
+ * selection_sortt - function to order whyt the method selection
+ *@array: value
+ *@size: value
+ * print_list - function to be printed
  * */
 void selection_sort(int *array, size_t size)
-{}
+{
+	size_t i, j, k;
+	int l;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+	for (i = 0 ; i < size - 1 ; i++)
+	{
+		k = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[k])
+			{
+				k = j;
+			}
+		}
+		if (i != k)
+		{
+			l = array[i];
+			array[i] = array[k];
+			array[k] = l;
+			print_array(array, size);
+		}
+	}
+}
